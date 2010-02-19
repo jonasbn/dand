39bf973a9142e1ecae6a3b7186612c21f55aa334
jonasbn <jonasbn@logiclab.dk>
2010-02-19 20:20:22 +0000

    DAND-6 #time 10m #comment Rolling back, we need a different strategy: svn merge -r 6979:6976
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/DAND/trunk@6980 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/WWW/DanDomain.pm
+++ lib/WWW/DanDomain.pm
@@ -7,12 +7,8 @@ use strict;
 use WWW::Mechanize;
 use WWW::Mechanize::Cached;
 use Carp qw(croak);
-use Class::Rebless;
 
-use WWW::DanDomain::Auth;
-use WWW::DanDomain::NoAuth;
-
-our $VERSION = '1.00';
+our $VERSION = '0.02';
 
 sub new {
     my ( $class, $param ) = @_;
@@ -26,25 +22,48 @@ sub new {
     } else {
         $mech = WWW::Mechanize->new( agent => $agent );
     }
-    $param->{mech} = $mech;
-    
-    my $self;
-    
-    if ($param->{username} || $param->{password}) {
-        if ((not $param->{username}) or (not $param->{password})) {
-            croak 'Both username and password is required for authentication';
-        }        
-        $self = WWW::DanDomain::Auth->new($param);
-        
-    } else {
-        $self = WWW::DanDomain::NoAuth->new($param);
-    }
 
-    if ($class !~ m/^WWW::DanDomain((?=::(NoAuth|Auth))|$)/) {
-        Class::Rebless->rebless($self, $class);
-    }
+    my $self = bless {
+        base_url => 'http://www.billigespil.dk/admin',
+        username => $param->{username},
+        password => $param->{password},
+        url      => $param->{url},
+        verbose  => $param->{verbose} || 0,
+        mech     => $mech,
+    }, $class;
 
     return $self;
+}
+
+sub retrieve {
+    my ( $self, $stat ) = @_;
+
+    $self->{mech}->get( $self->{base_url} )
+        or croak "Unable to retrieve base URL: $@";
+
+    $self->{mech}->submit_form(
+        form_number => 0,
+        fields      => {
+            UserName => $self->{username},
+            Password => $self->{password},
+        }
+    );
+
+    $self->{mech}->get( $self->{url} ) or croak "Unable to retrieve URL: $@";
+
+    my $content = $self->{mech}->content();
+
+    return $self->processor( \$content, $stat );
+}
+
+sub processor {
+    my ( $self, $content ) = @_;
+
+    if ( $self->{verbose} ) {
+        print STDERR "${$content}.\n";
+    }
+
+    return $content;
 }
 
 1;
@@ -55,11 +74,11 @@ __END__
 
 =head1 NAME
 
-WWW::DanDomain - factory for interacting with DanDomain admin interface
+WWW::DanDomain - class to assist in interacting with DanDomain admin interface
 
 =head1 VERSION
 
-This documentation describes version 1.00
+This documentation describes version 0.01
 
 =head1 SYNOPSIS
 
@@ -134,10 +153,9 @@ This can be used for automating tasks of processing data exports etc.
 
 =head1 DESCRIPTION
 
-This class is a factory for creating objects, which can interact with DanDomain's
-administrive web interface. It is basically just simple wrappers around
-L<WWW::Mechanize> it assists the user in getting going with automating tasks
-related to the DanDomain administrative web interface.
+This module is a simple wrapper around L<WWW::Mechnize> it assists the user
+in getting going with automating tasks related to the DanDomain administrative
+web interface.
 
 Such as:
 
@@ -148,7 +166,7 @@ columns)
 
 =item * filling in missing data (combining data)
 
-=item * converting formats (from CSV to XML / JSON / CSV and what not)
+=item * converting formats (from CSV to XML)
 
 =back
 
@@ -156,25 +174,16 @@ columns)
 
 =head2 new
 
-This is the constructor/factory.
+This is the constructor.
 
 The constructor takes a hash reference as input. The hash reference should
-contain parameters satisfying the required parameters for the object you want
-the factory to create.
-
-For both products of the factory:
+contain keys according to the following conventions:
 
 =over
 
-=item L<WWW::DanDomain::Auth>
+=item * username, the mandatory username to access DanDomain 
 
-=item L<WWW::DanDomain:.NoAuth>
-
-=back
-
-The following parameters are available:
-
-=over
+=item * password, the mandatory password to access DanDomain
 
 =item * url, the mandatory URL to retrieve data from (L</retrieve>)
 
@@ -195,34 +204,43 @@ The parameter is optional
 
 =back
 
-For L<WWW::DanDomain::NoAuth> the following additional parameters are required:
+=head2 retrieve
+
+Parameters:
 
 =over
 
-=item * username, the mandatory username to access DanDomain 
-
-=item * password, the mandatory password to access DanDomain
+=item * a hash reference, the reference can be populated with statistic
+information based on the lineprocessing (L</processor>) initiated from
+L</retrieve>.
 
 =back
 
-If these parameters are present, a L<WWW::DanDomain::Auth> object is returned.
-Both have to be present, if only one of them is present the construction of the
-object dies.
+The method returns a scalar reference to a string containing the content
+retrieved from the URL provided to the contructor (L</new>). If the
+L</processor> method is overwritten you can manipulate the content prior
+to being returned.
 
-If none of the above parameters are present a L<WWW::DanDomain::NoAuth> object
-is returned.
+=head2 processor
 
-This is the barebones logic of the factory, encapsulation of the actual
-construction.
+This is a stub and it might go away in the future. It does takes the content
+retrieved (see: L</retrieve>) from the URL parameter provided to the constructor
+(see: L</new>).
+
+Parameters:
+
+=over
+
+=item * a scalar reference to a string to be processed line by line
+
+=back
+
+The stub does however not do anything, but it returns the scalar reference
+I<untouched>.
 
 =head1 DIAGNOSTICS
 
 =over
-
-=item * Both username and password is required for authentication
-
-If you want the factory to provide a L<WWW::DanDomain::Auth> object you have to
-provide both B<username> and B<password>.
 
 =item * Unable to retrieve base URL: $@
 
@@ -262,7 +280,7 @@ with username and password is required.
 =head1 TEST AND QUALITY
 
 The tests are based on L<Test::MockObject::Extends> and example data are
-mocked dummy data. Please see the L</TODO> section.
+mocked dummy data. Please see the TODO section.
 
 The test suite uses the following environment variables as flags:
 
@@ -285,14 +303,9 @@ the distributions own test suite, instantiated the following way.
 ---------------------------- ------ ------ ------ ------ ------ ------ ------
 File                           stmt   bran   cond    sub    pod   time  total
 ---------------------------- ------ ------ ------ ------ ------ ------ ------
-blib/lib/WWW/DanDomain.pm      97.4   87.5   50.0  100.0  100.0   32.0   92.1
-...lib/WWW/DanDomain/Auth.pm  100.0  100.0    n/a  100.0  100.0   41.1  100.0
-...b/WWW/DanDomain/NoAuth.pm  100.0  100.0    n/a  100.0  100.0   17.5  100.0
-...ib/WWW/DanDomain/Super.pm  100.0    n/a  100.0  100.0  100.0    9.4  100.0
-Total                          99.0   92.9   62.5  100.0  100.0  100.0   96.7
+blib/lib/WWW/DanDomain.pm     100.0  100.0  100.0  100.0  100.0  100.0  100.0
+Total                         100.0  100.0  100.0  100.0  100.0  100.0  100.0
 ---------------------------- ------ ------ ------ ------ ------ ------ ------
-
-The data are based on version: 1.00
 
 =head1 QUALITY AND CODING STANDARD
 
@@ -314,24 +327,6 @@ L<Perl::Tidy> resource file, can be obtained from the original author
 =head1 BUGS AND LIMITATIONS
 
 No known bugs at this time.
-
-From version 1.00 and onwards the objects returned from the constructor  (L<WWW::DanDomain/new>) are not of the class: L<WWW::DanDomain>, they are either
-of the classes:
-
-=over
-
-=item * L<WWW::DanDomain::Auth>
-
-=item * L<WWW::DanDomain::NoAuth>
-
-=back
-
-Depending on the parameters provided, please see L</new> for details. Apart from
-the type returned methods and parameters are kept intact.
-
-It was considered to do a rebless (See: L<Class::Rebless/rebless>) so the original
-type was kept intact, but with the factory then also acting as SUPER class sounding
-like a bad idea and it was skipped again.
 
 =head1 BUG REPORTING
 
@@ -359,7 +354,7 @@ Please report any bugs or feature requests via:
 
 =item * Most of the work is done in the classes inheriting from this class,
 there could however be work to do in the maintenance area, making this class
-more informative when/if failing
+more informative if failing
 
 =item * I would like to add some integration test scripts so I can see that the
 package works with real data apart from the mock.
@@ -371,12 +366,6 @@ package works with real data apart from the mock.
 =over
 
 =item * L<http://www.dandomain.dk>
-
-=item * L<http://search.cpan.org/~gaas/libwww-perl/lib/HTTP/Status.pm>
-
-=item * L<http://search.cpan.org/perldoc?HTTP%3A%3AResponse>
-
-=item * L<http://www.logicLAB.org>
 
 =back
 
@@ -432,30 +421,22 @@ After some time I refactored to an object oriented structure making it even
 easier to maintain and adding more clients. This made the actual connectivity
 into a package (this package) letting it loose as open source.
 
-The system in which L<WWW::DanDomain> plays a small, but important part is
-constantly evolving and new features keep defining, so the OOP aspect showed
-to be a good strategy, major changes have however meant that it has been
-necessary to make some major leaps, introducing factors, which could prove
-to break backwards compatibility - but the most important aspect is to solve
-the task at hand.
-
 =head1 ACKNOWLEDGEMENTS
 
 =over
 
 =item * Andy Lester (petdance) the author of L<WWW::Mechanize> and
 L<WWW::Mechanize:Cached>, this module makes easy things easy and hard things
-possible
+possible.
 
-=item * Steen Schnack, understanding the power and flexibility of computer
-programming and custom solutions and who gave me the assignment in the first
-place
+=item * Steen Schnack, who understand the power and flexibility of computer
+programming and custom solutions and who gave me the assignment.
 
 =back
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2009-2010 jonasbn, all rights reserved.
+Copyright 2009 jonasbn, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
--- t/retrieve.t
+++ t/retrieve.t
@@ -4,7 +4,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 13;
+use Test::More tests => 6;
 use Carp;
 use Test::MockObject::Extends;
 use File::Slurp qw(slurp);
@@ -28,7 +28,6 @@ $mech->mock(
 );
 $mech->set_true('get', 'follow_link', 'submit_form');
 
-#Auth test
 my $content;
 
 $wd = WWW::DanDomain->new({
@@ -47,25 +46,6 @@ isa_ok($content, 'SCALAR');
 
 is($$content, 'test');
 
-#NoAuth test
-$content = '';
-
-$wd = WWW::DanDomain->new({
-	url      => 'http://www.billigespil.dk/admin/edbpriser-export.asp',
-    verbose  => $TEST_VERBOSE,
-    mech     => $mech,
-});
-
-can_ok($wd, qw(retrieve));
-
-ok($content = $wd->retrieve());
-
-isa_ok($content, 'SCALAR');
-
-is($$content, 'test');
-
-#Auth fails
-
 $mech->set_series('get', undef, undef);
 
 $wd = WWW::DanDomain->new({
@@ -83,43 +63,6 @@ $mech->set_series('get', 1, undef);
 $wd = WWW::DanDomain->new({
 	username => 'topshop',
 	password => 'topsecret',
-	url      => 'http://www.billigespil.dk/admin/edbpriser-export.asp',
-    verbose  => $TEST_VERBOSE,
-    mech     => $mech,
-});
-
-dies_ok { $content = $wd->retrieve(); };
-
-#Auth fails
-$mech->set_series('get', undef, undef);
-
-$wd = WWW::DanDomain->new({
-	username => 'topshop',
-	password => 'topsecret',
-	url      => 'http://www.billigespil.dk/admin/edbpriser-export.asp',
-    verbose  => $TEST_VERBOSE,
-    mech     => $mech,
-});
-
-dies_ok { $content = $wd->retrieve(); };
-
-$mech->set_series('get', 1, undef);
-
-$wd = WWW::DanDomain->new({
-	username => 'topshop',
-	password => 'topsecret',
-	url      => 'http://www.billigespil.dk/admin/edbpriser-export.asp',
-    verbose  => $TEST_VERBOSE,
-    mech     => $mech,
-});
-
-dies_ok { $content = $wd->retrieve(); };
-
-#NoAuth fails
-
-$mech->set_series('get', undef);
-
-$wd = WWW::DanDomain->new({
 	url      => 'http://www.billigespil.dk/admin/edbpriser-export.asp',
     verbose  => $TEST_VERBOSE,
     mech     => $mech,
